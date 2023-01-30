b2d2f424cd277ae90bb64e26dc3b9e18a31e948b
Ryzen32 <123226234+Ryzen32@users.noreply.github.com>
2023-01-30 22:48:13 +0530

    Add files via upload

--- backend/src/Routes/movieRoute.js
+++ backend/src/Routes/movieRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
+const { Movie } = require("../models/Moviemodel");
 const movieRoute = express.Router();
 
 movieRoute.get("/", async (req, res) => {
   try {
-    let movies = await movie.find();
+    let movies = await Movie.find();
     res.send({ msg: "send data", movies });
   } catch (err) {
     res.status(500).send({ msg: "failed !" });
